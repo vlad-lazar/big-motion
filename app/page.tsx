95f2ec6a51241285fffd169318868b7ed2ebfdd3
Vlad Lazar <vlla@monta.com>
2025-11-09 12:13:45 +0100

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-zinc-50 sm:items-start">
+        <button className="btn btn-primary">Hello DaisyUI</button>
+      </main>
+    </div>
+  );
+}
